85d9b3c4d85d6d9e33dbf82c0b57b329f5c7c0a8
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-10 14:32:58 -0400

    Comedy pull is finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Api from "./Api.js"
+import Comedy from './Comedy';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Api />
+        <Comedy />
       </div>
     );
   }
---
+++ src/Comedy.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import API_KEY from './config.js'
+
+
+export default class Comedy extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          comedyMovies: [],
+          comedyStore: []
+        }
+     
+      }
+ 
+
+      componentDidMount(){
+        const API_K = API_KEY;
+          for(let i = 1; i < 3; i++){
+         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${i}&primary_release_date.gte=1990&primary_release_date.lte=1999&with_genres=35`)
+        .then(json => {
+
+          this.setState({comedyMovies:json.data, comedyStore: json.data})
+
+          console.log(this.state.comedyMovies)
+     
+        })
+          }
+
+    }
+ 
+    render(){
+     
+return(
+<h1>hello</h1>
+
+ )
+ }
+}
