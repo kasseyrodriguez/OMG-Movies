171220905dd00313b0d047055277d0717ecb270e
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-10 15:31:34 -0400

    Home page is created

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Api from "./Api.js"
-import Comedy from './Comedy';
-import Romance from './Romance';
-import Horror from './Horror';
-import Animation from './Animation';
-import Action from './Action';
-import Drama from './Drama';
+import Home from "./Home.js"
+
+
 
 class App extends Component {
   render() {
     return (
+      <nav>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Api />
-        <Comedy />
-        <Romance />
-        <Horror />
-        <Animation />
-        <Action />
-        <Drama />
+      <Home />
       </div>
+      </nav>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Api from "./Api.js"
+import Comedy from './Comedy';
+import Romance from './Romance';
+import Horror from './Horror';
+import Animation from './Animation';
+import Action from './Action';
+import Drama from './Drama';
+
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Api />
+        <Comedy />
+        <Romance />
+        <Horror />
+        <Animation />
+        <Action />
+        <Drama />
+      </div>
+    );
+  }
+}
+
+export default Home;
