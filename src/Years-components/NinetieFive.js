2cee1b6220d8a1e749a3e1b7bc96e5e528efd588
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-14 19:06:32 -0400

    Data for 1995 fin

--- src/Years-components/NinetieFive.js
+++ src/Years-components/NinetieFive.js
@@ -1,8 +1,92 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import API_KEY from '../Home-components/config.js'
 
 
 
 export default class NinetieFive extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          NinetieFiveMovies: [],
+          NinetieFiveStore: [],
+          NinetieFiveRomance: [],
+          NinetieFivetAction: [],
+          NinetieFiveDrama: [],
+          NinetieFiveAnimation: [],
+          NinetieFiveHorror: [],
+          NinetieFiveComedy: [],
+        }
+     
+      }
+
+      componentDidMount(){
+        const API_K = API_KEY;
+          
+         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1995`)
+        .then(json => {
+
+          this.setState({NinetieFiveMovies:json.data, NinetieFiveStore: json.data})
+
+          console.log(this.state.NinetieFiveMovies)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1995&with_genres=10749`)
+        .then(json => {
+
+          this.setState({NinetieFiveRomance:json.data})
+
+          console.log(this.state.NinetieFiveRomance)
+     
+        })
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1995&with_genres=28`)
+        .then(json => {
+
+          this.setState({NinetieFivetAction:json.data})
+
+          console.log(this.state.NinetieFivetAction)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1995&with_genres=18`)
+        .then(json => {
+
+          this.setState({NinetieFiveDrama:json.data})
+
+          console.log(this.state.NinetieFiveDrama)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1995&with_genres=16`)
+        .then(json => {
+
+          this.setState({NinetieFiveAnimation:json.data})
+
+          console.log(this.state.NinetieFiveAnimation)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1995&with_genres=27`)
+        .then(json => {
+
+          this.setState({NinetieFiveHorror:json.data})
+
+          console.log(this.state.NinetieFiveHorror)
+     
+        })
+
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1995&with_genres=35`)
+        .then(json => {
+
+          this.setState({NinetieFiveComedy:json.data})
+
+          console.log(this.state.NinetieFiveComedy)
+     
+        })
+        
+    }
 
 
  
