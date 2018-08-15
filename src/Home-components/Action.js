f3d5eeca35bab114d72ac89683f19bec1493d7fa
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-15 15:50:28 -0400

    fixed rendering of view, not finished though

--- src/Home-components/Action.js
+++ src/Home-components/Action.js
@@ -17,8 +17,8 @@ export default class Action extends Component {
         const API_K = API_KEY;
          axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990&primary_release_date.lte=1999&with_genres=28`)
          .then(json => {
-           this.setState({actionMovies:json.data, actionStore: json.data})
-           console.log(this.state.actionMovies.results[0].title)
+           this.setState({actionMovies:json.data.results, actionStore: json.data})
+           console.log(this.state.actionMovies[0].title)
          })
       }
 
