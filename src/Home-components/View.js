390174e570782072aa6358fb96cb627fdc4945fd
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-15 15:13:29 -0400

    passing props

--- src/Home-components/View.js
+++ src/Home-components/View.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { element } from 'prop-types';
 
 
 export default class View extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
       }
 
     render(){
+
+        let actionMoviesvariable = this.props.actionMovies.map((element, index )=> {
+     console.log(element);
+     let infor = element.results;
+        });
+
+console.log(actionMoviesvariable)
+
       return(
 
      <div>
