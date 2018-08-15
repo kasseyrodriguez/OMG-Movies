fe72e3deb71d42884706fdaf1e30327d5c9e0c44
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-15 11:08:41 -0400

    Data for 1993 and 1992 done

--- src/Years-components/NinetieThree.js
+++ src/Years-components/NinetieThree.js
@@ -1,8 +1,91 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import API_KEY from '../Home-components/config.js'
 
 
 export default class NinetieThree extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          NinetieThreeMovies: [],
+          NinetieThreeStore: [],
+          NinetieThreeRomance: [],
+          NinetieThreeAction: [],
+          NinetieThreeDrama: [],
+          NinetieThreeAnimation: [],
+          NinetieThreeHorror: [],
+          NinetieThreeComedy: [],
+        }
+     
+      }
+
+      componentDidMount(){
+        const API_K = API_KEY;
+          
+         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1993`)
+        .then(json => {
+
+          this.setState({NinetieThreeMovies:json.data, NinetieThreeStore: json.data})
+
+          console.log(this.state.NinetieThreeMovies)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1993&with_genres=10749`)
+        .then(json => {
+
+          this.setState({NinetieThreeRomance:json.data})
+
+          console.log(this.state.NinetieThreeRomance)
+     
+        })
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1993&with_genres=28`)
+        .then(json => {
+
+          this.setState({NinetieThreeAction:json.data})
+
+          console.log(this.state.NinetieThreeAction)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1993&with_genres=18`)
+        .then(json => {
+
+          this.setState({NinetieThreeDrama:json.data})
+
+          console.log(this.state.NinetieThreeDrama)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1993&with_genres=16`)
+        .then(json => {
+
+          this.setState({NinetieThreeAnimation:json.data})
+
+          console.log(this.state.NinetieThreeAnimation)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1993&with_genres=27`)
+        .then(json => {
+
+          this.setState({NinetieThreeHorror:json.data})
+
+          console.log(this.state.NinetieThreeHorror)
+     
+        })
+
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1993&with_genres=35`)
+        .then(json => {
+
+          this.setState({NinetieThreeComedy:json.data})
+
+          console.log(this.state.NinetieThreeComedy)
+     
+        })
+        
+    }
 
 
  
--- src/Years-components/NinetieTwo.js
+++ src/Years-components/NinetieTwo.js
@@ -1,9 +1,91 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import API_KEY from '../Home-components/config.js'
 
 
 export default class NinetieTwo extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          NinetieTwoMovies: [],
+          NinetieTwoStore: [],
+          NinetieTwoRomance: [],
+          NinetieTwoAction: [],
+          NinetieTwoDrama: [],
+          NinetieTwoAnimation: [],
+          NinetieTwoHorror: [],
+          NinetieTwoComedy: [],
+        }
+     
+      }
 
+      componentDidMount(){
+        const API_K = API_KEY;
+          
+         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1992`)
+        .then(json => {
+
+          this.setState({NinetieTwoMovies:json.data, NinetieTwoStore: json.data})
+
+          console.log(this.state.NinetieTwoMovies)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1992&with_genres=10749`)
+        .then(json => {
+
+          this.setState({NinetieTwoRomance:json.data})
+
+          console.log(this.state.NinetieTwoRomance)
+     
+        })
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1992&with_genres=28`)
+        .then(json => {
+
+          this.setState({NinetieTwoAction:json.data})
+
+          console.log(this.state.NinetieTwoAction)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1992&with_genres=18`)
+        .then(json => {
+
+          this.setState({NinetieTwoDrama:json.data})
+
+          console.log(this.state.NinetieTwoDrama)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1992&with_genres=16`)
+        .then(json => {
+
+          this.setState({NinetieTwoAnimation:json.data})
+
+          console.log(this.state.NinetieTwoAnimation)
+     
+        })
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1992&with_genres=27`)
+        .then(json => {
+
+          this.setState({NinetieTwoHorror:json.data})
+
+          console.log(this.state.NinetieTwoHorror)
+     
+        })
+
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1992&with_genres=35`)
+        .then(json => {
+
+          this.setState({NinetieTwoComedy:json.data})
+
+          console.log(this.state.NinetieTwoComedy)
+     
+        })
+        
+    }
 
  
     render(){
