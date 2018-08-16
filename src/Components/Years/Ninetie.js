95c768703d130dc9e2e2b49942e80a3374603044
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-16 14:22:55 -0400

    1990s carousel is fin

--- src/Components/Years/Ninetie.js
+++ src/Components/Years/Ninetie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import API_KEY from '../../config.js'
+import Carousel from '../Genres/Carousel.js'
 
 
 export default class Ninetie extends Component {
@@ -15,13 +16,7 @@ export default class Ninetie extends Component {
           NinetieAnimation: [],
           NinetieHorror: [],
           NinetieComedy: [],
-          posters: [],
-          postersRomance: [],
-          postersAction:[],
-          postersDrama: [],
-          postersAnimation:[],
-          postersHorror: [],
-          postersComedy: []
+      
         }
       }
 
@@ -31,53 +26,47 @@ export default class Ninetie extends Component {
          axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1990`)
         .then(json => {
 
-          this.setState({NinetieMovies:json.data, NinetieStore: json.data})
+          this.setState({NinetieMovies:json.data.results, NinetieStore: json.data})
 
-          console.log(this.state.NinetieMovies)
-
+     
         })
 
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1990&with_genres=10749`)
         .then(json => {
 
-          this.setState({NinetieRomance:json.data})
+          this.setState({NinetieRomance:json.data.results})
 
-          console.log(this.state.NinetieRomance)
 
         })
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1990&with_genres=28`)
         .then(json => {
 
-          this.setState({NinetieAction:json.data})
+          this.setState({NinetieAction:json.data.results})
 
-          console.log(this.state.NinetieAction)
 
         })
 
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1990&with_genres=18`)
         .then(json => {
 
-          this.setState({NinetieDrama:json.data})
+          this.setState({NinetieDrama:json.data.results})
 
-          console.log(this.state.NinetieDrama)
 
         })
 
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1990&with_genres=16`)
         .then(json => {
 
-          this.setState({NinetieAnimation:json.data})
+          this.setState({NinetieAnimation:json.data.results})
 
-          console.log(this.state.NinetieAnimation)
 
         })
 
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1990&with_genres=27`)
         .then(json => {
 
-          this.setState({NinetieHorror:json.data})
+          this.setState({NinetieHorror:json.data.results})
 
-          console.log(this.state.NinetieHorror)
 
         })
 
@@ -85,9 +74,8 @@ export default class Ninetie extends Component {
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1990&with_genres=35`)
         .then(json => {
 
-          this.setState({NinetieComedy:json.data})
+          this.setState({NinetieComedy:json.data.results})
 
-          console.log(this.state.NinetieComedy)
 
         })
 
@@ -98,12 +86,26 @@ export default class Ninetie extends Component {
       return(
         <div>
           <h1>Most popular movies of 1990</h1>
+          <Carousel posters= {this.state.NinetieMovies} />
+
           <h1>Most popular Comedy of of 1990</h1>
+          <Carousel posters= {this.state.NinetieComedy} />
+
           <h1>Most popular romance of of 1990</h1>
+          <Carousel posters= {this.state.NinetieRomance} />
+
           <h1>Most popular horror of of 1990</h1>
+          <Carousel posters= {this.state.NinetieHorror} />
+
           <h1>Most popular animation of of 1990</h1>
+          <Carousel posters= {this.state.NinetieAnimation} />
+
           <h1>Most popular action of of 1990</h1>
+          <Carousel posters= {this.state.NinetieAction} />
+
           <h1>Most popular drama of of 1990</h1>
+          <Carousel posters= {this.state.NinetieDrama} />
+
         </div>
       )
     }
