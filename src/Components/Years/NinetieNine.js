ce78cc9e317041a6b44a0d8a47fb6b517d148357
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-16 14:51:55 -0400

    1999 carousel is done

--- src/Components/Years/NinetieNine.js
+++ src/Components/Years/NinetieNine.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import API_KEY from '../../config.js'
+import Carousel from '../Genres/Carousel.js'
 
 
 export default class NinetieNine extends Component {
@@ -25,53 +26,53 @@ export default class NinetieNine extends Component {
          axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1999`)
         .then(json => {
 
-          this.setState({NinetieNineMovies:json.data, NinetieNineStore: json.data})
+          this.setState({NinetieNineMovies:json.data.results, NinetieNineStore: json.data})
 
-          console.log(this.state.NinetieNineMovies)
+          // console.log(this.state.NinetieNineMovies)
 
         })
 
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1999&with_genres=10749`)
         .then(json => {
 
-          this.setState({NinetieNineRomance:json.data})
+          this.setState({NinetieNineRomance:json.data.results})
 
-          console.log(this.state.NinetieNineRomance)
+          // console.log(this.state.NinetieNineRomance)
 
         })
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1999&with_genres=28`)
         .then(json => {
 
-          this.setState({NinetieNineAction:json.data})
+          this.setState({NinetieNineAction:json.data.results})
 
-          console.log(this.state.NinetieNineAction)
+          // console.log(this.state.NinetieNineAction)
 
         })
 
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1999&with_genres=18`)
         .then(json => {
 
-          this.setState({NinetieNineDrama:json.data})
+          this.setState({NinetieNineDrama:json.data.results})
 
-          console.log(this.state.NinetieNineDrama)
+          // console.log(this.state.NinetieNineDrama)
 
         })
 
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1999&with_genres=16`)
         .then(json => {
 
-          this.setState({NinetieNineAnimation:json.data})
+          this.setState({NinetieNineAnimation:json.data.results})
 
-          console.log(this.state.NinetieNineAnimation)
+          // console.log(this.state.NinetieNineAnimation)
 
         })
 
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1999&with_genres=27`)
         .then(json => {
 
-          this.setState({NinetieNineHorror:json.data})
+          this.setState({NinetieNineHorror:json.data.results})
 
-          console.log(this.state.NinetieNineHorror)
+          // console.log(this.state.NinetieNineHorror)
 
         })
 
@@ -79,9 +80,9 @@ export default class NinetieNine extends Component {
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_K}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=1999&with_genres=35`)
         .then(json => {
 
-          this.setState({NinetieNineComedy:json.data})
+          this.setState({NinetieNineComedy:json.data.results})
 
-          console.log(this.state.NinetieNineComedy)
+          // console.log(this.state.NinetieNineComedy)
 
         })
 
@@ -92,13 +93,26 @@ export default class NinetieNine extends Component {
       return(
         
         <div>
-          <h1>Most popular movies of 1999</h1>
+        <h1>Most popular movies of 1999</h1>
+          <Carousel posters= {this.state.NinetieNineMovies} />
+
           <h1>Most popular Comedy of of 1999</h1>
+          <Carousel posters= {this.state.NinetieNineComedy} />
+
           <h1>Most popular romance of of 1999</h1>
+          <Carousel posters= {this.state.NinetieNineRomance} />
+
           <h1>Most popular horror of of 1999</h1>
+          <Carousel posters= {this.state.NinetieNineHorror} />
+
           <h1>Most popular animation of of 1999</h1>
+          <Carousel posters= {this.state.NinetieNineAnimation} />
+
           <h1>Most popular action of of 1999</h1>
+          <Carousel posters= {this.state.NinetieNineAction} />
+
           <h1>Most popular drama of of 1999</h1>
+          <Carousel posters= {this.state.NinetieNineDrama} />
         </div>
 
       )
