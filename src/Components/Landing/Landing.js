84ee6580a3dda1eccea4c8772f0efb6f846d5fa5
Anthony Clemow <anthony1487@gmail.com>
2018-08-16 16:51:46 -0400

    minor changes

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -13,7 +13,7 @@ class LandingPage extends React.Component {
     return (
       <div id="landingMain">
         <div class="row">
-    <div class="col-sm-9">
+    <div class="col-sm-9" id="tvIMG">
     <img src="https://i.imgur.com/ZE2fwus.png" className="img-fluid" alt="landingImg"/>
             <div class="col-sm-9" id="insideTV">
             <img src="https://m.media-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_SX300.jpg" className="img-fluid" id="otdIMG" alt="landingMovieIMG"/>
