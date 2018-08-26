6f3a51d66ff2a9fd47dd988958b59609de19e3e8
kasseyrodriguez <38043621+kasseyrodriguez@users.noreply.github.com>
2018-08-26 12:37:09 -0400

    Giphy kass (#48)
    * install express, get giphy, rearrange files, deleted some

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const axios = require("axios");
+
+app.get('/', (req, res) => res.send("Hello!"));
+
+var api = 'https://api.giphy.com/v1/gifs/search?';
+var apikey = "&api_key=############";
+var query = "&q=toy+story&limit=3";
+
+axios.get(api + apikey + query)
+.then(function (response) {
+  console.log(response.data);
+})
+
+
+app.listen(5000, ()=> console.log("App listening on port 5000"));
