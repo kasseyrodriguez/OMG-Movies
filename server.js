8f87dcbecb11646d05c54db1fdd9f447da2abe06
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-28 14:19:54 -0400

    grabs the giphy for the particular movie

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ const express = require('express');
 const app = express();
 const axios = require("axios");
 require('dotenv').load();
+var router = express.Router();
 
 var key = process.env.giphy_key
 var api = 'https://api.giphy.com/v1/gifs/search?';
 var apikey = `&api_key=${key}`;
-var query = "&q=toy+story&limit=3";
+// var query = "&q=toy+story&limit=3";
 
 
-app.get('/api', async (req, res) => {
-	let {data} = await axios.get(api + apikey + query);
+app.get('/api/:title', async (req, res) => {
+	console.log('requesting' + req.params.title);
+	let movie_url = api + apikey + `&q=${req.params.title}&limit=3`
+	console.log(movie_url);
+	let {data} = await axios.get(movie_url).catch((err)=>console.log(err));
+	console.log(data);
 	res.send(data);
+
 });
+
 
 
 app.listen(5000, ()=> console.log("App listening on port 5000"));
